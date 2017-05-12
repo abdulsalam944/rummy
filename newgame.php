caa0d39b579b57b3dce7e168dc7ea42111dc1546
root <you@example.com>
2017-05-12 11:46:50 +0530

    some changes

--- newgame.php
+++ newgame.php
@@ -1806,12 +1806,11 @@ experience
         var roomName;
         //CONNECTION
         var socket;
-<<<<<<< HEAD
-        //morning-island-26413.herokuapp.com
-        socket = io('wss://134.119.221.139:3000');
-=======
+
+
+
+
         socket = io('http://134.119.221.139:8080');
->>>>>>> 72af7178732afc33b0573533876838d419c9c1f8
         socket.on('connect', function() {            
               
             currentUsersId = socket.io.engine.id;
@@ -1827,15 +1826,15 @@ experience
 
               // console.log('my_user_id:',userid);
               //brodcast connection message
-<<<<<<< HEAD
+
               var msgToSend = {room:roomId,type:'code',msg:'re-connect',user:userid,oldid:userid,newid:socket.io.engine.id};
               socket.emit(eventName, JSON.stringify(msgToSend));  
             */
-=======
+
               // var msgToSend = {room:roomId,type:'code',msg:'re-connect',user:userid,oldid:userid,newid:socket.io.engine.id};
               // socket.emit(eventName, JSON.stringify(msgToSend));  
             
->>>>>>> 72af7178732afc33b0573533876838d419c9c1f8
+
         });
         function ConnectSocket(){            
             socket.emit('joinRoom', 'Connected.');
